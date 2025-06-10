e3424f4906212f9566a1e0e5233953299fe8830c
c0c2405474 <c0c2405474@edu.teu.ac.jp>
2025-06-10 15:47:39 +0900

    練習8:反転背景画像描画

--- pygame_image.py
+++ pygame_image.py
@@ -12,13 +12,14 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")#こうかとんの画像Surface
     kk_img=pg.transform.flip(kk_img,True,False)
+    bg_img2=pg.transform.flip(bg_img,True,False)
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         x += 1        
