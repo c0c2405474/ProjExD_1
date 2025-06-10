375b921174b981ae700681a6d761508f87abcc9b
c0c2405474 <c0c2405474@edu.teu.ac.jp>
2025-06-10 16:28:48 +0900

    演習1:何も押していないときの移動

--- pygame_image.py
+++ pygame_image.py
@@ -27,8 +27,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0)) 
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))                
-
+            kk_rct.move_ip((1,0)) 
+        else:
+            kk_rct.move_ip((-1,0)) 
         x=x%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
